a96cb78cbc5d8fbf31129d00d63fd1f3fc1d42f8
= <=>
2022-12-12 19:32:19 +0200

    added all necessary files

---
+++ api.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const XLSX = require('xlsx');
+const xlsxFile = require('read-excel-file/node');
+
+const workbook = XLSX.readFile("students.xlsx");
+const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+var students = [];
+
+xlsxFile('./students.xlsx').then((rows) => {
+  for (var i = 1; i < rows.length; i++) {
+    students.push({ name: rows[i][0], age: rows[i][1] });
+  }
+});
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500'
+}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.post('/addstudent', (req, res) => {
+  console.log(req.body);
+  XLSX.utils.sheet_add_aoa(worksheet, [[req.body.name, req.body.age]], { origin: -1 });
+  XLSX.writeFile(workbook, "students.xlsx");
+  students.push({ name: req.body.name, age: req.body.age });
+  res.send(students);
+})
+
+app.get('/data', function (req, res) {
+  res.send(students)
+})
+
+app.listen(3000)
